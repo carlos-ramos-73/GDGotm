a70857ca7dd25ea2daf2c510c1e02bdf2665bcad
Carlos Ramos <carlos.a.ramos.73@gmail.com>
2023-03-12 20:55:16 -0700

    impl/_GotmUserLocal.gd ported

---
+++ TestSuite/gotm/_impl/_GotmUserLocal.gd
@@ -0,0 +1,8 @@
+class_name _GotmUserLocal
+
+
+static func fetch(_path: String, _query: String = "", _params: Dictionary = {}, _authenticate: bool = false) -> Dictionary:
+	await _GotmUtility.get_tree().process_frame
+	return {}
+
+# TODO: This class seems useless. Need clarification of usage.
